ed6314bde780d8a09d1a37b3345b7ff030f838c0
Alessio Paccoia <alessiopcc91@gmail.com>
2026-04-18 15:26:45 +0200

    chore: 0.6.0

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'glide·sh v0.5.1',
+			title: 'glide·sh v0.6.0',
 			logo: {
 				dark: './public/logo-white.webp',
 				light: './public/logo-black.webp',
