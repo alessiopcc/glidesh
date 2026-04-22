41de6e87c39a0b44ea84a2661688b62ca45e9c8c
Alessio Paccoia <alessiopcc91@gmail.com>
2026-04-22 22:25:56 +0200

    refactor: drop redundant shell_escape rustdoc
    Function name and one-line body are self-explanatory.

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,3 @@
-/// Wrap a string in single quotes for safe shell interpolation.
-/// Embedded single quotes are escaped as `'\''`.
 pub fn shell_escape(s: &str) -> String {
     format!("'{}'", s.replace('\'', "'\\''"))
 }
